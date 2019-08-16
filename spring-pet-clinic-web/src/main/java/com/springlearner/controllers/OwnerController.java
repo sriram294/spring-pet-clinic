497b8429a7e1be9b30608ced665904b914bdc987
sriram294 <sriram294@gmail.com>
2019-08-16 23:29:35 +0530

    Create Vet and Owners index page and controller

---
+++ spring-pet-clinic-web/src/main/java/com/springlearner/controllers/OwnerController.java
@@ -0,0 +1,15 @@
+package com.springlearner.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"/","/index","/index.html"})
+    public String getOwners(){
+        return "owners/index";
+    }
+
+}
---
+++ spring-pet-clinic-web/src/main/java/com/springlearner/controllers/VetController.java
@@ -0,0 +1,15 @@
+package com.springlearner.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/vets")
+@Controller
+public class VetController {
+
+    @RequestMapping({"/","/index","/index.html"})
+    public String getVets(){
+        return "vets/index";
+    }
+
+}
